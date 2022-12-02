233a5c75e5c4520639003e2c2e9f1a09514efa8e
Ali Mehraj <amehraj360@gmail.com>
2022-12-02 13:17:21 +0200

    Add copyright comments to files

--- station_component/StationComponent.py
+++ station_component/StationComponent.py
@@ -1,4 +1,7 @@
-# Author(s): Chalith Tharuka <chalith.haputhantrige@tuni.fi>
+# Copyright 2022 Tampere University and VTT Technical Research Centre of Finland
+# This software was developed as a part of the ProCemPlus project: https://www.senecc.fi/projects/procemplus
+# This source code is licensed under the MIT license. See LICENSE in the repository root directory.
+# Author(s): Chalith Haputhantrige <chalith.haputhantrige@tuni.fi>
 
 import asyncio
 import traceback
